178b1acfa83a6c26b600eae2c7458db66522564e
Noah Synowiec <noahsyn1@gmail.com>
2021-02-02 00:48:41 -0500

    working on example

--- assets/example/example.py
+++ assets/example/example.py
@@ -19,7 +19,4 @@ def pythagoreanTheorem(a, b):
     if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
         return None
     return round(math.sqrt(a**2 + b**2), 4)
-
-
-
     
