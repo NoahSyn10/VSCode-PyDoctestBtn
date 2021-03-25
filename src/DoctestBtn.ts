6dd72bf8503bc6399264ae3ef2e25a4d7086422b
Noah Synowiec <noahsyn1@gmail.com>
2021-03-25 03:33:10 -0400

    Comment spacing

--- src/DoctestBtn.ts
+++ src/DoctestBtn.ts
@@ -44,7 +44,7 @@ export class DoctestBtn {
             Get data on doctests in file and update menu and status bar accordingly.
         */
         if ((docChange && docChange?.document.fileName !== activeEditor?.document.fileName) || activeEditor?.document.languageId !== "python") {
-            return;																				// Check if change was in the active editor & if the editor is a .py file
+            return;													                    // Check if change was in the active editor & if the editor is a .py file
         }
         
         this.utils.dualLog("> Scanning file for doctests...");
@@ -52,10 +52,10 @@ export class DoctestBtn {
         const docData = this.parser.countDoctests(activeEditor);
     
         if (docData?.totalDoctests > 0) {			
-            this.utils.dualLog("> " + docData.totalDoctests + " doctests found.");											// If there are doctests, show button and status bar items.                                                       
+            this.utils.dualLog("> " + docData.totalDoctests + " doctests found.");		// If there are doctests, show button and status bar items.                                                       
             this.config.showDoctestBtn(docData.totalDoctests);
     
-        } else {																				// If there are none, hide the button and status bar items.            
+        } else {							                                            // If there are none, hide the button and status bar items.            
             this.utils.dualLog("> No doctests found.");
             this.config.hideDoctestBtn();
         }
