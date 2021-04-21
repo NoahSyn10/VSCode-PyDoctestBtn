2c351265e530cf41a399721e9ad62955b7e1e57c
Noah Synowiec <noahsyn1@gmail.com>
2021-04-21 03:29:23 -0400

    Added a DoctestError class

--- src/Utils.ts
+++ src/Utils.ts
@@ -25,7 +25,37 @@ export class Utils {
         this.extOutput.appendLine(text);
     }
 
+    failureCallback (num: number, diag: DoctestFailure[]) {}
+
     singleNumCallback (output1: number) {}
 
     doubleNumCallback (output1: number, output2: number) {}
+}
+
+export class DoctestFailure {
+    /*
+        A class that encapsulates an error returned from doctest output.
+    */
+
+    rawTextList;
+    lineNum;
+    range;
+    errorMsg;
+
+    constructor (failure: string[], doc: vscode.TextDocument) {
+        this.rawTextList = failure;
+
+        // Get range.
+        this.lineNum = parseInt(failure[0].split(', ')[1].slice(5)) - 1;   // -1 accounts for indexing.
+        this.range = new vscode.Range(this.lineNum, doc.lineAt(this.lineNum).firstNonWhitespaceCharacterIndex,
+                                      this.lineNum, doc?.lineAt(this.lineNum).text.length);
+
+        // Get error message.
+        this.errorMsg = "Error message not available";  // In case an errorMsg is (somehow) not found.
+        if (failure.length === 7) {
+            this.errorMsg = "Expected: " + failure[4].trim() + "\nGot: " + failure[6].trim();
+        } else {
+            this.errorMsg = failure[failure.length-1].trim();
+        }
+    }
 }
